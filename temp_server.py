fa20973a52959e934f57622d7c887bf79564de2d
Leonard Scheidemantel <leonard@lsindustries.de>
2024-05-16 16:48:53 +0200

    fix: import error

--- temp_server.py
+++ temp_server.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify, send_file
 from io import BytesIO
 import torch
-from diffusers import StableDiffusionPipeline
+from diffusers import DiffusionPipeline
 
 app = Flask(__name__)
 
@@ -9,8 +9,7 @@ app = Flask(__name__)
 model_id = "SG161222/RealVisXL_V4.0"
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-pipe = StableDiffusionPipeline.from_pretrained(model_id,
-                                               torch_dtype=torch.float16 if device == "cuda" else torch.float32)
+pipe = DiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16 if device == "cuda" else torch.float32)
 pipe = pipe.to(device)
 
 
@@ -34,8 +33,7 @@ def generate_image():
     with torch.no_grad():
         for _ in range(num_images):
             generator = torch.manual_seed(42)
-            image = pipe(prompt, guidance_scale=cfg, generator=generator, num_inference_steps=50, height=height,
-                         width=width).images[0]
+            image = pipe(prompt, guidance_scale=cfg, generator=generator, height=height, width=width).images[0]
             img_io = BytesIO()
             image.save(img_io, 'PNG')
             img_io.seek(0)
