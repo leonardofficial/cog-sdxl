c87e4a049f0078cf1fc16a33867033154f3595a7
Leonard Scheidemantel <leonard@lsindustries.de>
2024-09-11 11:54:53 +0200

    feat: add logging to startup sequence

--- __main__.py
+++ __main__.py
@@ -1,3 +1,4 @@
+from helpers.logger import logger
 from rabbitmq.rabbitmq_fill import supabase_to_rabbitmq
 from temp_server_new import subscribe_to_queue_new
 from dotenv import load_dotenv
@@ -9,10 +10,11 @@ load_dotenv()
 MODE = os.getenv('MODE')
 
 if __name__ == "__main__":
-
     if MODE == 'consumer':
+        logger.info("Starting in consumer mode")
         subscribe_to_queue_new()
     elif MODE == 'filler':
+        logger.info("Starting in filler mode")
         supabase_to_rabbitmq()
     else:
         raise ValueError("Invalid mode")
