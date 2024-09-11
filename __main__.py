a18a89da2b90e8326cc8cc42e59931fb7cbafcf7
Leonard Scheidemantel <leonard@lsindustries.de>
2024-09-11 12:08:23 +0200

    feat: temporarily uncomment temp server code

--- __main__.py
+++ __main__.py
@@ -1,6 +1,5 @@
 from helpers.logger import logger
 from rabbitmq.rabbitmq_fill import supabase_to_rabbitmq
-from temp_server_new import subscribe_to_queue_new
 from dotenv import load_dotenv
 import os
 
@@ -14,7 +13,6 @@ if __name__ == "__main__":
 
     if MODE == 'consumer':
         logger.info("Starting in consumer mode")
-        subscribe_to_queue_new()
     elif MODE == 'filler':
         logger.info("Starting in filler mode")
         supabase_to_rabbitmq()
