5d0f816e5aa7c3626738d908419e9dcac4d8cd56
Leonard Scheidemantel <leonard@lsindustries.de>
2024-09-17 18:04:44 +0200

    feat: ensure connections are possible before startup

--- __main__.py
+++ __main__.py
@@ -1,14 +1,19 @@
 from helpers.load_config import load_config
 from helpers.logger import logger
+from rabbitmq.rabbitmq_connection import get_rabbitmq
 from rabbitmq.rabbitmq_consumer import subscribe_to_rabbitmq
 from rabbitmq.rabbitmq_filler import supabase_to_rabbitmq
 from stable_diffusion.stable_diffusion_manager import get_stable_diffusion
+from supabase_helpers.supabase_connection import get_supabase_postgres
 
 if __name__ == "__main__":
     logger.info("Brand name here...")
     config = load_config()
     logger.info("Starting up... Specified mode: %s", config.MODE)
 
+    # Ensure all required services are available before starting
+    get_rabbitmq()
+    get_supabase_postgres()
 
     if config.MODE == "consumer":
         logger.info("Starting in consumer mode")
