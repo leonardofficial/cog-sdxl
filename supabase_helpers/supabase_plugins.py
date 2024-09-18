31789cc7d50f5d7327dc8d64603756053b0d4720
Leonard Scheidemantel <leonard@lsindustries.de>
2024-09-18 19:54:05 +0200

    fix: change supabase client to supabase postgres for fetching plugins

--- supabase_helpers/supabase_plugins.py
+++ supabase_helpers/supabase_plugins.py
@@ -1,12 +1,14 @@
 from helpers.logger import logger
-from supabase_helpers.supabase_connection import get_supabase
+from supabase_helpers.supabase_connection import get_supabase, get_supabase_postgres
+
 
 def get_plugins_from_supabase():
     try:
         logger.info("Fetching an up-to-date list of plugins from Supabase...")
-        supabase = get_supabase()
-        plugins = supabase.table("plugins").select("id").execute()
-        return [plugin["id"] for plugin in plugins["data"]]
+        supabase = get_supabase_postgres()
+        plugins = supabase.from_("plugins").select("id").execute()
+        plugin_ids = [plugin["id"] for plugin in plugins.get("data", [])]
+        return plugin_ids
     except Exception as e:
         logger.exception("Failed to fetch plugins from Supabase: ", e)
         raise e
