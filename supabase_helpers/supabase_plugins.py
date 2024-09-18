212b92c81b16f22d656d402826e592c20ed25f98
Leonard Scheidemantel <leonard@lsindustries.de>
2024-09-18 19:46:20 +0200

    feat: implement fetching of plugins from supabase

--- supabase_helpers/supabase_plugins.py
+++ supabase_helpers/supabase_plugins.py
@@ -1,2 +1,12 @@
+from helpers.logger import logger
+from supabase_helpers.supabase_connection import get_supabase
+
 def get_plugins_from_supabase():
-    return ["f18b9084-0368-4c94-8e49-a52c3aafd2ef"]
+    try:
+        logger.info("Fetching an up-to-date list of plugins from Supabase...")
+        supabase = get_supabase()
+        plugins = supabase.table("plugins").select("id").execute()
+        return [plugin["id"] for plugin in plugins["data"]]
+    except Exception as e:
+        logger.exception("Failed to fetch plugins from Supabase: ", e)
+        raise e
