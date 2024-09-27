014e2e839d54e6a48211c69469842c410dbd2f17
Leonard Scheidemantel <leonard@lsindustries.de>
2024-09-27 20:49:37 +0200

    feat: update consts for stable diffusion

--- config/consts.py
+++ config/consts.py
@@ -1,3 +1,3 @@
-stable_diffusion_model_id = "RunDiffusion/Juggernaut-XI-v11"
+stable_diffusion_model_id = "RunDiffusion/Juggernaut-XL-v9"
 stable_diffusion_inference_steps = 30
 stable_diffusion_cfg = 7.5
